45b7641dee0f0dad87f614a42682c6be1fde542b
ekevin <etienne.kevin@gmail.com>
2011-05-11 16:29:15 +0100

    added empty service

---
+++ src/com/geoke/error404/Radio404Service.java
@@ -0,0 +1,15 @@
+package com.geoke.error404;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class Radio404Service extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
