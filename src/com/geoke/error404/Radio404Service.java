c64aa6749051b1b3fbd24be103258e2e61506874
ekevin <etienne.kevin@gmail.com>
2011-05-11 16:39:42 +0100

    added service

--- src/com/geoke/error404/Radio404Service.java
+++ src/com/geoke/error404/Radio404Service.java
@@ -5,6 +5,20 @@ import android.content.Intent;
 import android.os.IBinder;
 
 public class Radio404Service extends Service {
+	
+	private Boolean isPlaying = false;
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		// TODO Auto-generated method stub
+		return super.onStartCommand(intent, flags, startId);
+	}
+	
+	@Override
+	public void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
 
 	@Override
 	public IBinder onBind(Intent arg0) {
