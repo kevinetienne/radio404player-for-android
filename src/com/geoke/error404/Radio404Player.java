083d46a6f94e66ac96d63fd07cc0938f157aa336
Kevin <etienne.kevin@gmail.com>
2011-08-16 23:04:30 +0100

    added copyright

--- src/com/geoke/error404/Radio404Player.java
+++ src/com/geoke/error404/Radio404Player.java
@@ -1,4 +1,9 @@
 /*
+ * Radio404Player
+ * Version 1.0.1
+ * https://github.com/ekevin/radio404player
+ * Copyright (c) 2011 Kevin Etienne <etienne.kevin@gmail.com>
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
