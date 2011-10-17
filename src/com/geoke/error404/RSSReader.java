818c63555e37aeec4ef4b6ef6e1c0dce51afcc05
Kevin <etienne.kevin@gmail.com>
2011-10-17 23:09:08 +0100

    added empty RSSReader class

---
+++ src/com/geoke/error404/RSSReader.java
@@ -0,0 +1,23 @@
+/*
+ * Radio404Player
+ * Version 1.0.1
+ * https://github.com/ekevin/radio404player
+ * Copyright (c) 2011 Kevin Etienne <etienne.kevin@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.geoke.error404;
+
+class RSSReader {}
